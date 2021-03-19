efb8beee9293762607a08740c168660f3d58c34c
Gabriel Brito <gabrielbrito.dev@gmail.com>
2021-03-18 23:34:14 -0300

    Revisio - Day 1 - Variables, objects, arrays, primitive types and structural types.

---
+++ scrpts.js
@@ -0,0 +1,76 @@
+// Tipo de Dados
+
+// String
+const string = "Uma String"
+
+//Número
+const int = 1
+const float = 1.2
+
+//Booleano
+const isTrue = true
+const isFalse = false
+
+/*
+    Undefined e Null
+    
+    Null = Objeto vazio
+    Undefined = Não existe
+*/
+
+//Objeto
+const peaple = {
+    name: "Marcus",
+    age: 36,
+    walk: () => "Andando...",
+    run: () => "correndo...",
+}
+
+//Array
+const peaple = ["Gabriel", "Marcus", "João"]
+const numbers = [1, 3, 10, 25];
+
+/*
+    Tipos Primitovos: Imultáveis e não objetos.
+        1. String
+        2. NUmber
+        3. Boolean
+        4. Undefined
+        5. Symbol
+        6. BigInt
+
+    Tipos Estruturais: 
+        1. Object
+            *Array
+            *Map
+            *Set
+            *Date
+            *... 
+        2. Function
+
+    Tipos primitivos e estruturáis:
+        1. Null
+        
+*/
+
+
+
+
+// Variáveis
+
+// Var: Pode ser visto por todo escopo 
+var idade = 22
+idade = 23
+
+// Let: Só existe em um determinado escopo 
+var clima = "Quente"
+clima = "Frio"
+
+//Const: Funciona como o LET, mas não se pode atribuir novos valores ao mesmo
+var name = "Oliver"
+name = "Pedro"
+
+
+
+
+
